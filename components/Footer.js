1f05a0c1b76da8e4a22f06120e7434d16eb92a37
Jeevant <jeevantmudgil10@gmail.com>
2025-09-12 00:26:55 +0530

    Add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-purple-700 text-white text-center p-4 mt-8">
+      © {new Date().getFullYear()} Jeevant Portfolio. All rights reserved.
+    </footer>
+  );
+}
