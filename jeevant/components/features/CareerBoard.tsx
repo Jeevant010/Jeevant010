8ca4b72c087dab178e01f73cc2d1998c4a21ec64
Jeevant <jeevantmudgil10@gmail.com>
2026-07-07 04:12:04 +0530

    feat: replace PointerSensor with MouseSensor in CareerBoard component

--- jeevant/components/features/CareerBoard.tsx
+++ jeevant/components/features/CareerBoard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { DndContext, DragOverlay, closestCorners, KeyboardSensor, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { DndContext, DragOverlay, closestCorners, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { SortableContext, arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 import { updateStatus } from "@/lib/actions/career.actions";
 import { KanbanColumn } from "./KanbanColumn";
@@ -12,7 +12,7 @@ export function CareerBoard({ initialApplications }: { initialApplications: any[
   const [activeId, setActiveId] = useState<string | null>(null);
 
   const sensors = useSensors(
-    useSensor(PointerSensor, { activationConstraint: { distance: 5 } }),
+    useSensor(MouseSensor, { activationConstraint: { distance: 5 } }), useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 5 } }),
     useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
   );
 
